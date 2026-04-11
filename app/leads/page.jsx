35dd627b3a2b9f179dedb14db50329d42179e9cb
Pritam <pritam7599@gmail.com>
2026-04-11 18:27:18 +0530

    over all structure update

--- app/leads/page.jsx
+++ app/leads/page.jsx
@@ -1,22 +1,11 @@
 "use client"
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import Sidebar from '../../component/Sidebar'
-import Navbar from '../../component/Navbar'
+
 function page() {
   return (
-    <>
-
-      <div className="backgroundImage">
-        <Navbar />
-          <Sidebar />
-        <main>
-        </main>
-
-      </div>
-    </>
-  )
+    <p>
+Leads
+    </p>
+  );
 }
 
-export default page
+export default page;
--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,240 @@
 "use client"
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import Sidebar from '../component/Sidebar'
-import Navbar from '../component/Navbar'
+
 function page() {
   return (
-    <>
 
-      <div className="backgroundImage">
-        <Navbar />
-        <Sidebar />
-        <main>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat minus repellendus doloribus voluptatem magnam! Qui error, tempora possimus excepturi ad minus, libero dolorum molestias sequi unde illo non debitis quibusdam!</p>
-        </main>
+    <section className="container-fluid py-3">
+
+
+      {/* ROW 1  */}
+
+
+      <div className="row g-3 mb-3">
+
+  {/* Card 1 */}
+  <div className="col-12 col-md-6 col-xl-3">
+    <div className="card statCard stat-orange h-100">
+
+      <div className="card-body">
+
+        <div className="d-flex justify-content-between align-items-start">
+
+          <div>
+            <p className="statTitle">New Leads</p>
+
+            <h3 className="statValue">
+              158
+            </h3>
+
+            <p className="statGrowth text-success">
+              ▲ 12.4%
+            </p>
+          </div>
+
+          <div className="statArrow">
+            ↗
+          </div>
+
+        </div>
 
       </div>
-    </>
+
+      <div className="statBar bar-orange"></div>
+
+    </div>
+  </div>
+
+
+
+  {/* Card 2 */}
+  <div className="col-12 col-md-6 col-xl-3">
+    <div className="card statCard stat-green h-100">
+
+      <div className="card-body">
+
+        <div className="d-flex justify-content-between align-items-start">
+
+          <div>
+            <p className="statTitle">Deals Closed</p>
+
+            <h3 className="statValue">
+              34
+            </h3>
+
+            <p className="statGrowth text-success">
+              ▲ 9.1%
+            </p>
+          </div>
+
+          <div className="statArrow">
+            ↗
+          </div>
+
+        </div>
+
+      </div>
+
+      <div className="statBar bar-green"></div>
+
+    </div>
+  </div>
+
+
+
+  {/* Card 3 */}
+  <div className="col-12 col-md-6 col-xl-3">
+    <div className="card statCard stat-blue h-100">
+
+      <div className="card-body">
+
+        <div className="d-flex justify-content-between align-items-start">
+
+          <div>
+            <p className="statTitle">Revenue</p>
+
+            <h3 className="statValue">
+              $87,950
+            </h3>
+
+            <p className="statGrowth text-success">
+              ▲ 16.7%
+            </p>
+          </div>
+
+          <div className="statArrow">
+            ↗
+          </div>
+
+        </div>
+
+      </div>
+
+      <div className="statBar bar-blue"></div>
+
+    </div>
+  </div>
+
+
+
+  {/* Card 4 */}
+  <div className="col-12 col-md-6 col-xl-3">
+    <div className="card statCard stat-purple h-100">
+
+      <div className="card-body">
+
+        <div className="d-flex justify-content-between align-items-start">
+
+          <div>
+            <p className="statTitle">
+              Customer Satisfaction
+            </p>
+
+            <h3 className="statValue">
+              93%
+            </h3>
+
+            <span className="badge bg-success">
+              Excellent
+            </span>
+          </div>
+
+          <div className="statSmile">
+            🙂
+          </div>
+
+        </div>
+
+      </div>
+
+      <div className="statBar bar-purple"></div>
+
+    </div>
+  </div>
+
+</div>
+
+
+
+    
+      {/* ROW 2  */}
+
+
+      <div className="row g-3 mb-3">
+
+        {/* Pipeline */}
+        <div className="col-12 col-lg-8">
+          <div className="card dashboard-card h-100">
+            <div className="card-body">
+              Sales Pipeline
+            </div>
+          </div>
+        </div>
+
+        {/* Tasks */}
+        <div className="col-12 col-lg-4">
+          <div className="card dashboard-card h-100">
+            <div className="card-body">
+              Tasks Overview
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+
+
+       
+      {/* ROW 3  */}
+       
+
+      <div className="row g-3 mb-3">
+
+        <div className="col-12 col-lg-4">
+          <div className="card dashboard-card h-100">
+            <div className="card-body">
+              Lead Conversion Rate
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 col-lg-4">
+          <div className="card dashboard-card h-100">
+            <div className="card-body">
+              Top Opportunities
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 col-lg-4">
+          <div className="card dashboard-card h-100">
+            <div className="card-body">
+              Recent Activities
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+
+
+       
+      {/* ROW 4*/}
+       
+
+      <div className="row g-3">
+
+        <div className="col-12 col-lg-4">
+          <div className="card dashboard-card h-100">
+            <div className="card-body">
+              Upcoming Meetings
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </section>
+
   )
 }
 
